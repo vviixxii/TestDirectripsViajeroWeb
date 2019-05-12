7e22f49768811201369ce62c12d28fb011975454
vviixxii <39810963+vviixxii@users.noreply.github.com>
2019-05-12 01:48:44 -0500

    Update BaseTest.java

--- src/test/java/directripsViajeroWeb/core/BaseTest.java
+++ src/test/java/directripsViajeroWeb/core/BaseTest.java
@@ -26,7 +26,15 @@ public class BaseTest {
 	public static void initialization() {
 		//System.setProperty("webdriver.chrome.driver", "libChrome//chromedriver"); LOCAL
 		System.setProperty("webdriver.chrome.driver", "//home//jenkinsserver//node_modules//chromedriver//lib//chromedriver//chromedriver");
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
+		//driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 	}
 	
 	public void takeSnapShot(String fileWithPath) throws Exception {
