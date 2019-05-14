9ef070807a5b70b6ea6b87bddec81ce0a15a4405
vviixxii <39810963+vviixxii@users.noreply.github.com>
2019-05-14 10:51:37 -0500

    Update BaseTest.java

--- src/test/java/directripsViajeroWeb/core/BaseTest.java
+++ src/test/java/directripsViajeroWeb/core/BaseTest.java
@@ -80,10 +80,10 @@ public class BaseTest {
 			options.addArguments("start-maximized"); // open Browser in maximized mode
 			options.addArguments("disable-infobars"); // disabling infobars
 			options.addArguments("--disable-extensions"); // disabling extensions
-			options.addArguments("--disable-gpu"); // applicable to windows os only
+			//options.addArguments("--disable-gpu"); // applicable to windows os only
 			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 			options.addArguments("--no-sandbox"); // Bypass OS security model
-			options.addArguments("--headless"); // No GUI
+			//options.addArguments("--headless"); // No GUI
 			options.setExperimentalOption("useAutomationExtension", false);
 			driver = new ChromeDriver(options);
 		}
